afed7a48d33c67659f240d35e28980625ca929cf
Rijk van Putten <rijkvanputten@hotmail.nl>
2020-02-10 17:30:12 +0100

    Enemy

--- src/game.rs
+++ src/game.rs
@@ -1,15 +1,16 @@
 extern crate sdl2;
 
+use std::path::Path;
 use sdl2::event::Event;
 use sdl2::keyboard::Keycode;
-use sdl2::pixels::{Color, PixelFormatEnum};
-use sdl2::rect::{Point, Rect, };
+use sdl2::rect::{Point, Rect};
 use std::time::Duration;
+use sdl2::pixels::{Color, PixelFormatEnum};
 
 pub fn main() -> Result<(), String> {
     let sdl_context = sdl2::init()?;
     let video_subsystem = sdl_context.video()?;
-
+   
     let window = video_subsystem.window("Rust Game", 1000, 800)
         .position_centered()
         .resizable()
@@ -18,10 +19,16 @@ pub fn main() -> Result<(), String> {
 
     let mut canvas = window.into_canvas().build().map_err(|e| e.to_string())?;
 
+    let texture_creator = canvas.texture_creator();
+    let temp_surface = sdl2::surface::Surface::load_bmp(Path::new("assets/enemy1.bmp"))?;
+    let texture = texture_creator.create_texture_from_surface(&temp_surface)
+        .map_err(|e| e.to_string())?;
+
     let mut event_pump = sdl_context.event_pump()?;
 
-    let mut player_point = Point::new(100, 100);
-    
+    let mut player_point = Point::new(500, 500);
+    let mut enemy_point = Point::new(200, 200);
+
     let mut move_left = false;
     let mut move_right = false;
     const FPS: f64 = 60.0;
@@ -68,6 +75,21 @@ pub fn main() -> Result<(), String> {
         let player_rect = Rect::from_center(player_point, 64, 64);
         canvas.set_draw_color(Color::RGBA(255, 50, 50, 255));
         canvas.fill_rect(player_rect).map_err(|e| e.to_string())?;
+
+        let mut dirX: f64 = (player_point.x - enemy_point.x) as f64;
+        let mut dirY: f64 = (player_point.y - enemy_point.y) as f64;
+        use std::num;
+        let sqr: f64 = dirX * dirX + dirY * dirY;
+        let mag = sqr.sqrt();
+        dirX /= mag;
+        dirY /= mag;
+        dirX *= 100.0 * DELTA_TIME;
+        dirY *= 100.0 * DELTA_TIME;
+        enemy_point.x += dirX as i32;
+        enemy_point.y += dirY as i32;
+
+        canvas.copy(&texture, None, Rect::from_center(enemy_point, 64, 64))?;
+        canvas.present();
 
         canvas.present();
 
