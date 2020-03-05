b852b56bf314071f18974ef0567daeb975ef237b
Rijk-van-Putten <rijkvanputten@hotmail.nl>
2020-03-05 20:53:15 +0100

    Working on dungeons generation...

--- src/world.rs
+++ src/world.rs
@@ -5,10 +5,13 @@ use crate::tile::TileInfo;
 use rand::Rng;
 use sdl2::render::Texture;
 
-const WORLD_SIZE: usize = 30;
+const DUNGEON_SIZE: usize = 5;
+const TILES_PER_DUNGEON: usize = 10;
+const WORLD_SIZE: usize = DUNGEON_SIZE * TILES_PER_DUNGEON;
 
 pub struct World<'a> {
-    nodes: [[u16; WORLD_SIZE]; WORLD_SIZE],
+    dungeon_map: [[u16; DUNGEON_SIZE]; DUNGEON_SIZE],
+    tile_map: [[u16; WORLD_SIZE]; WORLD_SIZE],
     tile_data: [TileInfo<'a>; 2]
 }
 
@@ -20,13 +23,24 @@ impl World<'_> {
         ];
 
         World{
-            nodes: [[0;WORLD_SIZE];WORLD_SIZE],
+            dungeon_map: [[0;DUNGEON_SIZE];DUNGEON_SIZE],
+            tile_map: [[0;WORLD_SIZE];WORLD_SIZE],
             tile_data
         }
     }
 
     pub fn generate(&mut self) {
+        // Dungeon map
         let mut rng = rand::thread_rng();
+        for x in 0..DUNGEON_SIZE
+        {
+            for y in 0..DUNGEON_SIZE
+            {
+                self.dungeon_map[x][y] = rng.gen_range(0, 2);
+            }
+        }
+
+        // Tile map
         let mut fill_counter = 0;
         let total_filled_nodes = (WORLD_SIZE * WORLD_SIZE) / 2;
         println!("NODES: {}", total_filled_nodes);
@@ -34,9 +48,9 @@ impl World<'_> {
         {
             let random_x = rng.gen_range(0, WORLD_SIZE);
             let random_y = rng.gen_range(0, WORLD_SIZE);
-            if self.nodes[random_x][random_y] != 1
+            if self.tile_map[random_x][random_y] != 1
             {
-                self.nodes[random_x][random_y] = 1;
+                self.tile_map[random_x][random_y] = 1;
                 fill_counter += 1;
             }
         }
@@ -49,7 +63,14 @@ impl World<'_> {
 
     pub fn log_world(&self)
     {
-        for (_y, row) in self.nodes.iter().enumerate() {
+        for (_y, row) in self.dungeon_map.iter().enumerate() {
+            println!("");
+            for (_x, col) in row.iter().enumerate() {
+                print!("{}", col);
+            }
+        }
+
+        for (_y, row) in self.tile_map.iter().enumerate() {
             println!("");
             for (_x, col) in row.iter().enumerate() {
                 print!("{}", col);
@@ -59,7 +80,7 @@ impl World<'_> {
 
     pub fn get_tile(&self, x: usize, y: usize) -> u16
     {
-        self.nodes[x][y]
+        self.tile_map[x][y]
     }
 
     pub fn get_texture(&self, tile_id: u16) -> &Texture<'_>
